d8c019a51a3203425c82ebd380fdb3a88b16daec
Nikolay Denev <ndenev@ndenev.com>
2025-12-27 12:18:30 -0600

    Add debug output for IN list multi-cluster test

--- tests/integration/tests/02-multi-cluster.sh
+++ tests/integration/tests/02-multi-cluster.sh
@@ -38,6 +38,11 @@ assert_contains "IN list returns current cluster pods" "k3d-k8sql-test-1" \
     "k3d-k8sql-test-1"
 
 # IN list should also return cluster 2's pods
+# Debug: show actual output to diagnose multi-cluster issue
+echo "DEBUG: IN list query result:"
+$K8SQL -c "k3d-k8sql-test-1" -q "SELECT name, _cluster FROM pods WHERE _cluster IN ('k3d-k8sql-test-1', 'k3d-k8sql-test-2') AND namespace = 'default'" -o json 2>&1 || true
+echo ""
+
 assert_contains "IN list returns cluster 2 pods" "k3d-k8sql-test-1" \
     "SELECT name, _cluster FROM pods WHERE _cluster IN ('k3d-k8sql-test-1', 'k3d-k8sql-test-2') AND namespace = 'default'" \
     "k3d-k8sql-test-2"
